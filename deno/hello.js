432631da41c237c259974884b038097ec63e0e4b
沈鑫 <kouding.sx@antgroup.com>
2021-07-19 20:30:06 +0800

    submit

---
+++ deno/hello.js
@@ -0,0 +1,9 @@
+
+function fullName (firstName) {
+  return "hello " + firstName;
+}
+
+export default {
+  name: 'shenxin',
+  fullName: fullName("wangya"),
+}
---
+++ deno/test.ts
@@ -0,0 +1,8 @@
+import { print } from 'https://deno.land/x/walle_store@alpha-2/index.ts';
+// https://github.com/LDWX/study-practise/releases/tag/0.0.1-rc.1
+
+
+// @VERSION/FILE_PATH.
+// import test from './index.ts';
+console.log('hello test');
+print();
